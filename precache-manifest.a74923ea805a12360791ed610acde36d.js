c7896c3e5a69341bde30574fabaf8cd166a8fe28
pnaing@mail.sfsu.edu <pnaing@mail.sfsu.edu>
2018-12-21 15:09:43 -0800

    Updates

---
+++ precache-manifest.a74923ea805a12360791ed610acde36d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fbb9c57c6c3e77c0fa0",
+    "url": "/Project_Omail/static/js/runtime~main.4fbb9c57.js"
+  },
+  {
+    "revision": "febb661d3aff35e93e52",
+    "url": "/Project_Omail/static/js/main.febb661d.chunk.js"
+  },
+  {
+    "revision": "4fb894a59d241b6c20b2",
+    "url": "/Project_Omail/static/js/1.4fb894a5.chunk.js"
+  },
+  {
+    "revision": "febb661d3aff35e93e52",
+    "url": "/Project_Omail/static/css/main.e4c63162.chunk.css"
+  },
+  {
+    "revision": "d684198807ae159fe80322adb602f0d6",
+    "url": "/Project_Omail/index.html"
+  }
+];
